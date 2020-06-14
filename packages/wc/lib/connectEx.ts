a6c17fb1aaae4598f1323bd10d6ffa54c64f2f11
tpluscode <tpluscode@users.noreply.github.com>
2020-06-14 12:45:26 +0200

    lint before build

--- packages/wc/lib/connectEx.ts
+++ packages/wc/lib/connectEx.ts
@@ -1,4 +1,4 @@
-import {connect, Constructor, Connectable, Store, ModelsState} from '@captaincodeman/rdx'
+import { connect, Constructor, Connectable, Store, ModelsState } from '@captaincodeman/rdx'
 import * as models from '@hydrofoil/shaperone-core/state/models'
 
 export function connectEx<T extends Constructor<Connectable>, SS extends Store<ModelsState<typeof models>>>(
