515a9f911bf0e6c74683924919e7058935d49137
tpluscode <tpluscode@users.noreply.github.com>
2020-06-16 21:08:38 +0200

    add missing mixins

--- packages/core/state/datasets/effects.ts
+++ packages/core/state/datasets/effects.ts
@@ -3,6 +3,23 @@ import type { SingleContextClownface } from 'clownface'
 import { Shape } from '@rdfine/shacl'
 import { FocusNode } from '../../index'
 import rdfine from '@tpluscode/rdfine'
+import type { Mixin } from '@tpluscode/rdfine/lib/ResourceFactory'
+
+async function loadMixins(): Promise<Mixin[]> {
+  const deps = [
+    import('@rdfine/shacl/dependencies/Shape.js').then(s => s.ShapeDependencies),
+    import('@rdfine/shacl/dependencies/PropertyGroup.js').then(s => s.PropertyGroupDependencies),
+  ]
+
+  const mixins = await Promise.all(deps)
+
+  return mixins.reduce<Mixin[]>((flat, mixins) => {
+    return [
+      ...flat,
+      ...Object.values(mixins),
+    ]
+  }, [])
+}
 
 export function effects(store: Store) {
   const dispatch = store.dispatch()
@@ -23,9 +40,9 @@ export function effects(store: Store) {
 
       let shape: Shape
 
+      const mixins = await loadMixins()
+      rdfine.factory.addMixin(...mixins)
       if ('_context' in shapeOrPointer) {
-        const { ShapeDependencies } = (await import('@rdfine/shacl/dependencies/Shape.js'))
-        rdfine.factory.addMixin(...Object.values(ShapeDependencies))
         shape = rdfine.factory.createEntity(shapeOrPointer)
       } else {
         shape = shapeOrPointer
