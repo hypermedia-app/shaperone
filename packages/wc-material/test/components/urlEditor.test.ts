0c9b32c73b7fd3ec52c81fbfc3a1a93c680c0051
tpluscode <tpluscode@users.noreply.github.com>
2020-10-30 14:20:58 +0100

    test(url-editor): add update tests

--- packages/wc-material/test/components/urlEditor.test.ts
+++ packages/wc-material/test/components/urlEditor.test.ts
@@ -1,6 +1,8 @@
 import { fixture, expect } from '@open-wc/testing'
 import cf from 'clownface'
 import $rdf from '@rdf-esm/dataset'
+import { TextField } from '@material/mwc-textfield'
+import * as sinon from 'sinon'
 import { urlEditor } from '../../components/urlEditor'
 import { editorTestParams } from '../util'
 
@@ -17,5 +19,26 @@ describe('wc-material/components/urlEditor', () => {
 
     // then
     expect(element).to.equalSnapshot()
+  })
+
+  it('renders field[type=url]', async () => {
+    // given
+    const graph = cf({ dataset: $rdf.dataset() })
+    const { params, actions } = editorTestParams({
+      object: graph.namedNode('foo-bar'),
+    })
+    await Promise.all(urlEditor.loadDependencies!())
+    const element = await fixture<TextField>(urlEditor.render(params, actions))
+
+    // when
+    element.focus()
+    element.value = 'http://example.com/'
+    element.blur()
+
+    // then
+    expect(actions.update).to.have.been.calledOnceWith(sinon.match({
+      value: 'http://example.com/',
+      termType: 'NamedNode',
+    }))
   })
 })
--- packages/wc-vaadin/test/components/urlEditor.test.ts
+++ packages/wc-vaadin/test/components/urlEditor.test.ts
@@ -1,6 +1,8 @@
 import cf from 'clownface'
 import $rdf from '@rdf-esm/dataset'
 import { expect, fixture } from '@open-wc/testing'
+import * as sinon from 'sinon'
+import { TextFieldElement } from '@vaadin/vaadin-text-field'
 import { editorTestParams } from '../util'
 import { urlEditor } from '../../components/url-editor'
 
@@ -17,5 +19,26 @@ describe('wc-vaadin/components/url-editor', () => {
 
     // then
     expect(element).to.equalSnapshot()
+  })
+
+  it('renders field[type=url]', async () => {
+    // given
+    const graph = cf({ dataset: $rdf.dataset() })
+    const { params, actions } = editorTestParams({
+      object: graph.namedNode('foo-bar'),
+    })
+    await Promise.all(urlEditor.loadDependencies!())
+    const element = await fixture<TextFieldElement>(urlEditor.render(params, actions))
+
+    // when
+    element.focus()
+    element.value = 'http://example.com/'
+    element.blur()
+
+    // then
+    expect(actions.update).to.have.been.calledOnceWith(sinon.match({
+      value: 'http://example.com/',
+      termType: 'NamedNode',
+    }))
   })
 })
