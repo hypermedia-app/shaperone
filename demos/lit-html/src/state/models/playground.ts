6339b95cc0ef477283afd3092b9c17d485d88c13
tpluscode <tpluscode@users.noreply.github.com>
2020-11-04 09:28:23 +0100

    feat: include rendering settings in sharing link

--- demos/lit-html/src/state/models/playground.ts
+++ demos/lit-html/src/state/models/playground.ts
@@ -1,5 +1,7 @@
 import { createModel } from '@captaincodeman/rdx'
 import type { Store } from '../store'
+import { ComponentsState } from './components'
+import { RendererState } from './renderer'
 
 export interface State {
   sharePopup: boolean
@@ -7,7 +9,7 @@ export interface State {
 }
 
 interface SharingParam {
-  key: 'shapes' | 'shapesFormat' | 'resource' | 'resourceFormat' | 'resourcePrefixes'
+  key: 'shapes' | 'shapesFormat' | 'resource' | 'resourceFormat' | 'resourcePrefixes' | keyof ComponentsState | keyof RendererState
   value: string
 }
 
@@ -55,6 +57,18 @@ export const playground = createModel({
       'shape/format': function (value: string) {
         dispatch.playground.updateSharingParams({ key: 'shapesFormat', value })
       },
+      'componentsSettings/switchComponents': function (value: string) {
+        dispatch.playground.updateSharingParams({ key: 'components', value })
+      },
+      'componentsSettings/setEditorChoice': function (value: boolean) {
+        dispatch.playground.updateSharingParams({ key: 'disableEditorChoice', value: value.toString() })
+      },
+      'rendererSettings/switchNesting': function (value: string) {
+        dispatch.playground.updateSharingParams({ key: 'nesting', value })
+      },
+      'rendererSettings/switchLayout': function (value: string) {
+        dispatch.playground.updateSharingParams({ key: 'grouping', value })
+      },
       updateSharingParams({ key, value }: SharingParam) {
         const { playground: state } = store.getState()
 
@@ -64,12 +78,16 @@ export const playground = createModel({
         dispatch.playground.setSharingLink(sharingLink.toString())
       },
       restoreState() {
-        const sharedState = new URLSearchParams(document.location.search)
+        const sharedState: Map<SharingParam['key'], string> = new URLSearchParams(document.location.search) as any
         const shapes = sharedState.get('shapes')
         const shapesFormat = sharedState.get('shapesFormat')
         const resource = sharedState.get('resource')
         const resourceFormat = sharedState.get('resourceFormat')
         const resourcePrefixes = sharedState.get('resourcePrefixes')
+        const layout = sharedState.get('grouping')
+        const nesting = sharedState.get('nesting')
+        const disableEditorChoice = sharedState.get('disableEditorChoice')
+        const components = sharedState.get('components')
 
         if (shapes && shapesFormat) {
           dispatch.shape.format(shapesFormat)
@@ -81,6 +99,18 @@ export const playground = createModel({
           if (resourcePrefixes) {
             dispatch.resource.setPrefixes(resourcePrefixes.split(','))
           }
+        }
+        if (layout) {
+          dispatch.rendererSettings.switchLayout(layout as any)
+        }
+        if (nesting) {
+          dispatch.rendererSettings.switchNesting(nesting as any)
+        }
+        if (disableEditorChoice) {
+          dispatch.componentsSettings.setEditorChoice(disableEditorChoice === 'true')
+        }
+        if (components) {
+          dispatch.componentsSettings.switchComponents(components as any)
         }
       },
     }
