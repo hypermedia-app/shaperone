36b8801ad1d72aefddbee20a9d2ff09e141c12d3
tpluscode <tpluscode@users.noreply.github.com>
2020-12-14 11:29:38 +0100

    feat(core): sync properties which are sh:equals

---
+++ packages/core/models/forms/effects/updateObject.ts
@@ -0,0 +1,20 @@
+import { UpdateObjectParams } from '../reducers/updateObject'
+import type { Store } from '../../../state'
+import { syncProperties } from './lib/syncProperties'
+
+export function updateObject(store: Store) {
+  const dispatch = store.getDispatch()
+
+  return ({ form, focusNode, property }: Pick<UpdateObjectParams, 'form' | 'focusNode' | 'property'>) => {
+    const { forms, editors } = store.getState()
+
+    syncProperties({
+      dispatch,
+      editors,
+      forms,
+      form,
+      focusNode,
+      property,
+    })
+  }
+}
---
+++ packages/core/test/models/forms/effects/setPropertyObjects.test.ts
@@ -0,0 +1,47 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import cf from 'clownface'
+import $rdf from 'rdf-ext'
+import { dash, schema, sh } from '@tpluscode/rdf-ns-builders'
+import { Store } from '../../../../state'
+import { testFocusNodeState, testPropertyState, testStore } from '../util'
+import { setPropertyObjects } from '../../../../models/forms/effects/setPropertyObjects'
+import { nodeShape, propertyShape } from '../../../util'
+
+describe('models/forms/effects/setPropertyObjects', () => {
+  let store: Store
+  let form: symbol
+
+  beforeEach(() => {
+    ({ form, store } = testStore())
+  })
+
+  it('dispatches update of property linked by sh:equals', () => {
+    // given
+    const focusNode = cf({ dataset: $rdf.dataset() }).namedNode('foo')
+    const shapesGraph = cf({ dataset: $rdf.dataset() })
+    const property = propertyShape(shapesGraph.blankNode(), {
+      path: schema.name,
+    })
+    const synced = propertyShape(shapesGraph.blankNode(), {
+      [dash.hidden.value]: true,
+      path: schema.givenName,
+      [sh.equals.value]: schema.name,
+    })
+    store.getState().forms.get(form)!.focusNodes = testFocusNodeState(focusNode, {
+      shape: nodeShape(shapesGraph.blankNode()),
+      properties: [testPropertyState(property.pointer), testPropertyState(synced.pointer)],
+    })
+
+    // when
+    setPropertyObjects(store)({
+      form,
+      focusNode,
+      property,
+    })
+
+    // then
+    const dispatch = store.getDispatch()
+    expect(dispatch.forms.setPropertyObjects).to.have.been.called
+  })
+})
