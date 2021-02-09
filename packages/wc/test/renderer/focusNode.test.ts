8e2aedabb8c0f62e17b437993d53cba9cda887a0
tpluscode <tpluscode@users.noreply.github.com>
2021-02-09 18:49:58 +0100

    test: focus node renderer

---
+++ packages/wc/test/renderer/focusNode.test.ts
@@ -0,0 +1,52 @@
+import { testFocusNode } from '@shaperone/testing/models/form'
+import { sinon } from '@shaperone/testing'
+import { FormRenderer } from '@hydrofoil/shaperone-core/renderer'
+import { FocusNode } from '@hydrofoil/shaperone-core'
+import { fixture, html, expect } from '@open-wc/testing'
+import { Dispatch } from '@hydrofoil/shaperone-core/state'
+import { renderFocusNode } from '../../renderer/focusNode'
+import { formRenderer } from '../../../testing/renderer'
+
+describe('wc/renderer/focusNode', () => {
+  let focusNode: FocusNode
+  let renderer: sinon.SinonStubbedInstance<FormRenderer>
+  let dispatch: sinon.SinonStubbedInstance<Dispatch['forms']>
+
+  beforeEach(() => {
+    focusNode = testFocusNode().focusNode
+    renderer = formRenderer()
+    dispatch = renderer.context.dispatch.forms as any
+  })
+
+  it('dispatches to create node when necessary', async () => {
+    // given
+    renderer.context.templates.initialising = () => html`Loading`
+
+    // when
+    const result = await fixture(html`<div>${renderFocusNode.call(renderer, { focusNode })}</div>`)
+
+    // then
+    expect(result.textContent).to.eq('Loading')
+    expect(dispatch.createFocusNodeState).to.have.been.calledWith(sinon.match({
+      focusNode: sinon.match({
+        term: focusNode.term,
+      }),
+    }))
+  })
+
+  it('calls render template', async () => {
+    // given
+    const childState = testFocusNode()
+    renderer.context.state.focusNodes[focusNode.value] = childState
+    renderer.context.templates.focusNode = sinon.stub().returns(html`Focus node`)
+
+    // when
+    const result = await fixture(html`<div>${renderFocusNode.call(renderer, { focusNode })}</div>`)
+
+    // then
+    expect(result.textContent).to.eq('Focus node')
+    expect(renderer.context.templates.focusNode).to.have.been.calledWith(sinon.match({
+      focusNode: childState,
+    }))
+  })
+})
