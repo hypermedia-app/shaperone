d0e7e4025bfe09104b5e279e7571a594b20773e3
tpluscode <tpluscode@users.noreply.github.com>
2021-02-11 12:15:50 +0100

    feat: lib function to traverse property simple paths

--- packages/core/lib/property.ts
+++ packages/core/lib/property.ts
@@ -1,6 +1,7 @@
-import { Term } from 'rdf-js'
+import { NamedNode, Term } from 'rdf-js'
 import { sh } from '@tpluscode/rdf-ns-builders'
 import { literal, namedNode } from '@rdf-esm/data-model'
+import { GraphPointer, MultiPointer } from 'clownface'
 import type { PropertyState } from '../models/forms'
 
 export function createTerm(property: Pick<PropertyState, 'shape' | 'datatype'>, value: string): Term {
@@ -9,4 +10,43 @@ export function createTerm(property: Pick<PropertyState, 'shape' | 'datatype'>, 
   }
 
   return literal(value, property.datatype)
+}
+
+/**
+ * Finds all nodes connected to the input node by following a [SHACL Property Path](https://www.w3.org/TR/shacl/#dfn-shacl-property-path)
+ *
+ * @param node starting node
+ * @param shPath SHACL Property Path
+ */
+export function findNodes(node: MultiPointer, shPath: GraphPointer | NamedNode): MultiPointer {
+  const path = 'termType' in shPath ? node.node(shPath) : shPath
+
+  const list = path.list()
+  if (list) {
+    const seq = [...list].map(p => p.term)
+    return seq.reduce((current, nextPath) => current.out(nextPath), node)
+  }
+
+  if (path.term.termType === 'BlankNode') {
+    if (path.out(sh.inversePath).term) {
+      return node.in(path.out(sh.inversePath).term)
+    }
+
+    if (path.out(sh.alternativePath).term) {
+      const list = path.out(sh.alternativePath).list()
+      if (list) {
+        const alt = [...list].map(p => p.term)
+        return node.out(alt)
+      }
+      return node.out([])
+    }
+
+    if (path.out([sh.zeroOrMorePath, sh.oneOrMorePath]).term) {
+      throw new Error('sh:zeroOrMorePath and sh:oneOrMorePath are not supported')
+    }
+
+    throw new Error(`Unrecognized property path ${path.value}`)
+  }
+
+  return node.out(shPath)
 }
--- packages/core/test/lib/property.test.ts
+++ packages/core/test/lib/property.test.ts
@@ -2,39 +2,111 @@ import { describe, it } from 'mocha'
 import { expect } from 'chai'
 import cf from 'clownface'
 import $rdf from 'rdf-ext'
-import { sh, xsd } from '@tpluscode/rdf-ns-builders'
-import { createTerm } from '../../lib/property'
+import { sh, xsd, schema } from '@tpluscode/rdf-ns-builders'
+import { any, blankNode, namedNode } from '@shaperone/testing/nodeFactory'
+import { ex as tbbt } from '@shaperone/testing'
+import { createTerm, findNodes } from '../../lib/property'
 import { propertyShape } from '../util'
 
 describe('core/lib/property', () => {
-  it('creates named node when property has sh:IRI kind', () => {
+  describe('createTerm', () => {
+    it('creates named node when property has sh:IRI kind', () => {
     // given
-    const pointer = cf({ dataset: $rdf.dataset() }).blankNode()
-    const property = {
-      shape: propertyShape(pointer, {
-        [sh.nodeKind.value]: sh.IRI,
-      }),
-    }
+      const pointer = cf({ dataset: $rdf.dataset() }).blankNode()
+      const property = {
+        shape: propertyShape(pointer, {
+          [sh.nodeKind.value]: sh.IRI,
+        }),
+      }
 
-    // when
-    const term = createTerm(property, 'http://foo/bar')
+      // when
+      const term = createTerm(property, 'http://foo/bar')
 
-    // then
-    expect(term).to.deep.equal($rdf.namedNode('http://foo/bar'))
+      // then
+      expect(term).to.deep.equal($rdf.namedNode('http://foo/bar'))
+    })
+
+    it('creates typed literal when property has sh:datatype', () => {
+    // given
+      const pointer = cf({ dataset: $rdf.dataset() }).blankNode()
+      const property = {
+        shape: propertyShape(pointer),
+        datatype: xsd.int,
+      }
+
+      // when
+      const term = createTerm(property, '41')
+
+      // then
+      expect(term).to.deep.equal($rdf.literal('41', xsd.int))
+    })
   })
 
-  it('creates typed literal when property has sh:datatype', () => {
-    // given
-    const pointer = cf({ dataset: $rdf.dataset() }).blankNode()
-    const property = {
-      shape: propertyShape(pointer),
-      datatype: xsd.int,
-    }
+  describe('findNodes', () => {
+    const graph = any()
 
-    // when
-    const term = createTerm(property, '41')
+    const sheldon = graph.namedNode(tbbt.Sheldon)
+      .addOut(schema.knows, [tbbt.Penny, tbbt.Howard, tbbt.Amy, tbbt.Leonard])
 
-    // then
-    expect(term).to.deep.equal($rdf.literal('41', xsd.int))
+    const penny = graph.namedNode(tbbt.Penny)
+
+    const leonard = graph.namedNode(tbbt.Leonard)
+      .addOut(schema.spouse, tbbt.Penny)
+      .addOut(schema.knows, tbbt.Sheldon)
+
+    it('follows direct path as named node', () => {
+      // given
+      const path = schema.knows
+
+      // when
+      const nodes = findNodes(sheldon, path)
+
+      // then
+      expect(nodes.terms).to.deep.contain.members([tbbt.Penny, tbbt.Howard, tbbt.Amy, tbbt.Leonard])
+    })
+
+    it('follows direct path as pointer', () => {
+      // given
+      const path = schema.knows
+
+      // when
+      const nodes = findNodes(sheldon, namedNode(path))
+
+      // then
+      expect(nodes.terms).to.deep.contain.members([tbbt.Penny, tbbt.Howard, tbbt.Amy, tbbt.Leonard])
+    })
+
+    it('follows simple inverse path', () => {
+      // given
+      const path = blankNode().addOut(sh.inversePath, schema.spouse)
+
+      // when
+      const nodes = findNodes(penny, path)
+
+      // then
+      expect(nodes.term).to.deep.eq(leonard.term)
+    })
+
+    it('follows simple alternative path', () => {
+      // given
+      const path = blankNode().addList(sh.alternativePath, [schema.spouse, schema.knows])
+
+      // when
+      const nodes = findNodes(leonard, path)
+
+      // then
+      expect(nodes.terms).to.deep.contain.members([tbbt.Penny, tbbt.Sheldon])
+    })
+
+    it('follows simple sequence path', () => {
+      // given
+      const [path] = blankNode().addList(sh.path, [schema.knows, schema.spouse]).out(sh.path).toArray()
+
+      // when
+      const nodes = findNodes(sheldon, path)
+
+      // then
+      expect(nodes.term).to.deep.eq(tbbt.Penny)
+    })
   })
 })
