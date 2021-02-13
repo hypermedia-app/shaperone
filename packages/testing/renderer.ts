9f647fa7ab9ff2e9d08a1f4b3b4e535c7bd82dcf
tpluscode <tpluscode@users.noreply.github.com>
2021-02-13 13:41:20 +0100

    test: missing mock

--- packages/testing/renderer.ts
+++ packages/testing/renderer.ts
@@ -91,6 +91,7 @@ export const propertyRenderer = ({ property, ...arg }: TestProperty): sinon.Sino
       addObject: sinon.spy(),
       selectMultiEditor: sinon.spy(),
       selectSingleEditors: sinon.spy(),
+      removeObject: sinon.spy(),
     },
   }
 }
