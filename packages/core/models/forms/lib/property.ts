f3450d016d82a31c4489f3badae939ce3e39ffed
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-03-31 12:30:30 +0200

    fix: also support property shape direct in logical constraint

--- packages/core/models/forms/lib/property.ts
+++ packages/core/models/forms/lib/property.ts
@@ -1,4 +1,6 @@
 import type { PropertyShape, Shape } from '@rdfine/shacl'
+import { sh } from '@tpluscode/rdf-ns-builders'
+import { fromPointer } from '@rdfine/shacl/lib/PropertyShape'
 
 export function canAddObject(property: PropertyShape, length: number): boolean {
   return length < (property.maxCount || Number.POSITIVE_INFINITY)
@@ -8,11 +10,23 @@ export function canRemoveObject(property: PropertyShape, length: number): boolea
   return length > (property.minCount || 0)
 }
 
+function isPropertyShape(shape: Shape): shape is PropertyShape {
+  return shape.pointer.out(sh.path).terms.length > 0
+}
+
+function getProperties(shape: Shape): PropertyShape[] {
+  if (isPropertyShape(shape)) {
+    return [fromPointer(shape.pointer)]
+  }
+
+  return shape.property
+}
+
 export function combineProperties(shape: Shape): PropertyShape[] {
   return [
     shape,
     ...shape.and,
     ...shape.or,
     ...shape.xone,
-  ].flatMap(shape => shape.property)
+  ].flatMap(getProperties)
 }
