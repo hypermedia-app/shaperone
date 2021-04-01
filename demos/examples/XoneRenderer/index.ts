1d77fc2bd5ac4f7a75f0d9665b9544228a634f69
tpluscode <tpluscode@users.noreply.github.com>
2021-04-01 19:50:06 +0200

    refactor: simplify add some code comments to example code

--- demos/examples/XoneRenderer/index.ts
+++ demos/examples/XoneRenderer/index.ts
@@ -2,34 +2,53 @@ import { FocusNodeTemplate } from '@hydrofoil/shaperone-wc/templates'
 import { html } from 'lit-html'
 import { LogicalConstraint } from '@hydrofoil/shaperone-core/models/forms'
 import type { Shape } from '@rdfine/shacl'
-import TermMap from '@rdf-esm/term-map'
 import { Term } from 'rdf-js'
 
-const selectedShapes = new TermMap<Term, Shape>()
+const selectedShapes = new WeakMap<Term, Shape>()
 
-function renderDropdown(onSelected: (xone: LogicalConstraint, shape: Shape) => void) {
-  return function (xone: LogicalConstraint) {
-    return html`<select @input="${(e: any) => onSelected(xone, xone.shapes[(e.target).selectedIndex])}">
+function renderDropdown(shapeSelected: (xone: LogicalConstraint, shape: Shape) => void) {
+  return (xone: LogicalConstraint) => {
+    function onInput(e: any) {
+      shapeSelected(xone, xone.shapes[(e.target).selectedIndex])
+    }
+
+    return html`<select @input="${onInput}">
       ${xone.shapes.map(shape => html`<option>${shape.label || shape.id.value}</option>`)}
     </select>`
   }
 }
 
+/**
+ * This function decorates a {@link FocusNodeTemplate} by rendering select menus for every `sh:xone` logical
+ * constraint found on the node shape. By switching the selected item in the dropdown, all properties within a
+ * `sh:xone` group will be hidden with the exception of the one selected.
+ *
+ * Hiding supports property shapes as well as node shapes which group multiple properties
+ *
+ * @param focusNodeTemplate
+ */
 export function focusNode(focusNodeTemplate: FocusNodeTemplate): FocusNodeTemplate {
-  const renderer: FocusNodeTemplate = function (context, args) {
+  const renderer: FocusNodeTemplate = (context, args) => {
     const { focusNode: { logicalConstraints: { xone } }, actions } = context
 
     for (const group of xone) {
+      // on initial render make sure that only the first shape from every
+      // sh:xone is shown and hide the rest
+      // using a WeakMap to prevent memory leaking when the form is removed from the page
       const selectedShape = selectedShapes.get(group.term.term)
       if (!selectedShape) {
-        group.shapes.forEach(shape => actions.hideProperty(shape))
-        actions.showProperty(group.shapes[0])
+        const [first, ...rest] = group.shapes
+        actions.showProperty(first)
+        rest.forEach(shape => actions.hideProperty(shape))
+
         selectedShapes.set(group.term.term, group.shapes[0])
       }
     }
 
     function select(xone: LogicalConstraint, shape: Shape) {
+      // when a shape is selected from, hide all properties
       xone.shapes.forEach(shape => actions.hideProperty(shape))
+      // and then show then show the selected
       actions.showProperty(shape)
     }
 
