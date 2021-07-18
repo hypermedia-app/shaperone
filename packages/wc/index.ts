f3342b3c832112234866cf625e88bc1bc95d2105
tpluscode <tpluscode@users.noreply.github.com>
2021-07-18 21:45:15 +0200

    docs: update links to lit

--- packages/wc/index.ts
+++ packages/wc/index.ts
@@ -14,16 +14,16 @@ export { html, css } from 'lit'
 export type { SingleEditor, MultiEditor, Lazy } from '@hydrofoil/shaperone-core'
 
 /**
- * Inherit to implement a `dash:SingleEditor` component rendered as Web Components using [lit](https://lit.polymer-project.org/)
+ * Inherit to implement a `dash:SingleEditor` component rendered as Web Components using [lit](https://lit.dev/)
  */
 export type SingleEditorComponent<TState extends ComponentInstance = ComponentInstance> = Core.SingleEditorComponent<TState, TemplateResult>
 
 /**
- * Inherit to implement a `dash:MultiEditor` component rendered as Web Components using [lit](https://lit.polymer-project.org/)
+ * Inherit to implement a `dash:MultiEditor` component rendered as Web Components using [lit](https://lit.dev/)
  */
 export type MultiEditorComponent<TState extends ComponentInstance = ComponentInstance> = Core.MultiEditorComponent<TState, TemplateResult>
 
 /**
- * Function interface for declaring a component's `.render` function which returns a [lit](https://lit.polymer-project.org/) template result
+ * Function interface for declaring a component's `.render` function which returns a [lit](https://lit.dev/) template result
  */
 export type Render<TComponent extends Component = Component> = Core.RenderComponent<TComponent, TemplateResult>
