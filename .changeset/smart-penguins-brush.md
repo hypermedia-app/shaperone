374792b575f75fd36f0900ce62c797c928189bd5
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-08-29 15:02:10 +0200

    fix: missing imports

---
+++ .changeset/smart-penguins-brush.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/shaperone-wc-shoelace": patch
+---
+
+Dependencies not imported by multi-instances editor
--- packages/wc-shoelace/component-extras.ts
+++ packages/wc-shoelace/component-extras.ts
@@ -3,15 +3,7 @@ import {
 } from '@hydrofoil/shaperone-core/components.js'
 import * as select from '@hydrofoil/shaperone-core/lib/components/base/instancesSelect.js'
 import sh1 from '@hydrofoil/shaperone-core/ns.js'
-import { html } from 'lit'
-import { repeat } from 'lit/directives/repeat.js'
 import { Lazy, MultiEditorComponent } from '@hydrofoil/shaperone-wc'
-import { difference } from '@ngard/tiny-difference'
-import { SlSelect } from '@shoelace-style/shoelace'
-import type { GraphPointer } from 'clownface'
-import { renderItem } from './lib/components.js'
-import { stop } from './lib/handlers.js'
-import { settings } from './settings.js'
 
 export const instancesMultiSelectEditor: Lazy<MultiEditorComponent> = {
   ...select,
@@ -20,38 +12,8 @@ export const instancesMultiSelectEditor: Lazy<MultiEditorComponent> = {
     return instancesSelect.init?.call(this, ...args) || true
   },
   async lazyRender() {
-    return ({ property, componentState }, { update }) => {
-      const values = property.objects.map(o => o.object?.value).filter(isDef)
-      const pointers: GraphPointer[] = componentState.instances || []
+    const { render } = await import('./components/multiInstancesSelect.js')
 
-      function onChange(e: CustomEvent) {
-        const target = e.target as SlSelect
-
-        if (Array.isArray(target.value) && difference(target.value, values).length !== 0) {
-          const selected = pointers
-            .filter(({ value }) => target.value.includes(value))
-            .map(({ term }) => term)
-
-          update(selected)
-        }
-      }
-
-      function selectAll() {
-        const all = pointers.map(({ term }) => term)
-        update(all)
-      }
-
-      return html`
-        <sl-select ?hoist="${settings.hoist}" multiple clearable .value=${values} @sl-hide=${stop} @sl-change=${onChange}>
-          ${repeat(pointers || [], renderItem)}
-        </sl-select>
-        <sl-button @click=${selectAll}>
-          Select all
-        </sl-button>`
-    }
+    return render
   },
-}
-
-function isDef<T>(x: T | undefined): x is T {
-  return !!x
 }
