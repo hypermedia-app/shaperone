30dafb4b080cd9ad52c137793b4c548c4242a4a7
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-09-13 16:07:52 +0200

    fix: multiselect freeze

--- packages/wc-shoelace/lib/difference.ts
+++ packages/wc-shoelace/lib/difference.ts
@@ -1,6 +1,12 @@
+/* eslint-disable */
+
 // copy of @ngard/tiny-difference to have it ESM
 export function difference(array: unknown[], ...args: unknown[]) {
-  const otherArrays = Array.prototype.slice.call(args, 1)
-  if (!Array.isArray(array) || !otherArrays.every(Array.isArray)) return []
-  return array.filter(element => otherArrays.every(otherArray => otherArray.indexOf(element) === -1))
+  const otherArrays = Array.prototype.slice.call(arguments, 1);
+  if (!Array.isArray(array) || !otherArrays.every(Array.isArray)) return [];
+  return array.filter(function(element) {
+    return otherArrays.every(function(otherArray) {
+      return otherArray.indexOf(element) === -1;
+    });
+  });
 }
