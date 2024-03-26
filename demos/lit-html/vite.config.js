f0da2f08f1fcf3d0307f5a9854ebfc43941dfd3e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-03-26 16:37:25 +0100

    build: local URL

--- demos/lit-html/vite.config.js
+++ demos/lit-html/vite.config.js
@@ -1,8 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { mergeConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
 // eslint-disable-next-line import/no-relative-packages
 import config from '../../vite.config.js'
 
-export default mergeConfig(config, {
-  base: '/playground/',
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return mergeConfig(config, {
+      base: '/playground/',
+    })
+  }
+
+  return config
 })
