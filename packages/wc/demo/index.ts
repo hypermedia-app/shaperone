4f758ca881be8f01119646279ef5e8b638b595b5
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-11-28 09:53:52 +0100

    refactor: async config

--- packages/wc/demo/index.ts
+++ packages/wc/demo/index.ts
@@ -1,9 +1,11 @@
 import 'api-viewer-element/lib/api-docs.js'
-import '../shaperone-form.js'
+import { configure } from '../configure.js'
 
-import('../custom-elements.json').then((data: any) => {
-  const demoViewer = document.querySelector('api-docs')
-  if (demoViewer) {
-    demoViewer.elements = data.tags
-  }
-})
+configure()
+  .then(() => import('../custom-elements.json'))
+  .then((data: any) => {
+    const demoViewer = document.querySelector('api-docs')
+    if (demoViewer) {
+      demoViewer.elements = data.tags
+    }
+  })
