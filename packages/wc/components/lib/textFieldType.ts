27e95eaabf716d488e5d2ab0f21eb5d4bbb0e2a6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-11-28 12:17:07 +0100

    fix(wc): decimal input

--- packages/wc/components/lib/textFieldType.ts
+++ packages/wc/components/lib/textFieldType.ts
@@ -1,9 +1,15 @@
-import { numericDatatype } from '@hydrofoil/shaperone-core/lib/datatypes.js'
+import { numericDatatypeKind } from '@hydrofoil/shaperone-core/lib/datatypes.js'
+import type { NamedNode } from '@rdfjs/types'
 
-export function getType(datatype: ReturnType<typeof numericDatatype> | undefined) {
-  if (numericDatatype(datatype)) {
-    return 'number'
+export function getType(datatype: NamedNode | undefined) {
+  const numericDatatype = numericDatatypeKind(datatype)
+
+  if (numericDatatype) {
+    return {
+      type: 'number',
+      step: numericDatatype === 'integer' ? undefined : 'any',
+    }
   }
 
-  return 'text'
+  return { type: 'text' }
 }
