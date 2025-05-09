6f71050914d98afbbdaebbec6b6145be76f428a1
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-05-09 11:02:33 +0200

    docs: fix story of extending matchers

--- demos/storybook/stories/Editors/Matchers/Matchers.stories.ts
+++ demos/storybook/stories/Editors/Matchers/Matchers.stories.ts
@@ -1,7 +1,8 @@
 import type { StoryObj as Story } from '@storybook/web-components'
 import { createStory, defaultMeta } from '../../common.js'
-import textField from '../../../shapes/editors/matchers/star-rating.ttl?raw'
-import starRating from '../../../data/rating.ttl?raw'
+import starRatingShapes from '../../../shapes/editors/matchers/star-rating.ttl?raw'
+import textField from '../../../shapes/editors/dash/TextField/rdfs.ttl?raw'
+import starRatingData from '../../../data/rating.ttl?raw'
 import { preferMultiline } from './ExtendingMatchers.js'
 import { starRatingMatcher } from './AddingMatchers.js'
 
@@ -10,8 +11,8 @@ const meta = { ...defaultMeta }
 export default meta
 
 export const AddingMatchers: Story = createStory({
-  shapes: textField,
-  data: starRating,
+  shapes: starRatingShapes,
+  data: starRatingData,
   focusNode: 'http://example.org/rating',
 })(({ editors }) => {
   editors.addMatchers({
