196b22c94bfdb956d1f691aa244b200bb37f50cd
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-05-16 15:29:50 +0200

    test: removed material

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -27,7 +27,6 @@ const config = {
   groups: [
     { name: 'hydra', files: 'packages/hydra/test/**/*.test.ts' },
     { name: 'wc', files: 'packages/wc/test/**/*.test.ts' },
-    { name: 'wc-material', files: 'packages/wc-material/test/**/*.test.ts' },
     { name: 'wc-vaadin', files: 'packages/wc-vaadin/test/**/*.test.ts' },
     { name: 'wc-shoelace', files: 'packages/wc-shoelace/test/**/*.test.ts' },
   ],
